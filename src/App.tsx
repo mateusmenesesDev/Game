29fb797d4acc61d214e94d2b6b5907959d9066f6
Mateus <mateusppa10@gmail.com>
2022-12-23 14:09:26 -0300

    Resolvendo conflito

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Header } from './components/Header';
 import { Genres } from './components/Genres';
 import { ContextProvider } from './contexts/Context';
-import { Placeholder } from './components/utils/Placeholder';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Header />
       <main>
         <Genres />
-        <Placeholder quantify={16} />
       </main>
     </ContextProvider>
   );
