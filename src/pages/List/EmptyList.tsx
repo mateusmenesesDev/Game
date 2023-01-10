5a0f48eaf2d65a6e4055297ca4a70fe17994b4cb
Mateus <mateusppa10@gmail.com>
2023-01-10 11:08:02 -0300

    :sparkles: feat: Componente de lista vazia

---
+++ src/pages/List/EmptyList.tsx
@@ -0,0 +1,7 @@
+export default function EmptyList() {
+  return (
+    <div>
+      <div className="text-center">Sua lista está vazia :(</div>
+    </div>
+  );
+}
--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,62 +1,75 @@
 import { useContext, useState } from "react";
 import { Context } from "../../contexts/Context";
+import EmptyList from "./EmptyList";
 import ListGameData from "./ListGameData";
 
 export function List() {
   const { userList } = useContext(Context);
   const [tab, setTab] = useState("All");
   return (
-    <div className="">
-      <div className="tabs flex-nowrap overflow-auto sm:justify-center">
-        <a
-          className={`tab tab-bordered ${tab === "All" && "tab-active"}`}
-          onClick={() => setTab("All")}
-        >
-          All
-        </a>
-        <a
-          className={`tab tab-bordered ${tab === "Playing" && "tab-active"}`}
-          onClick={() => setTab("Playing")}
-        >
-          Playing
-        </a>
-        <a
-          className={`tab tab-bordered ${tab === "Completed" && "tab-active"}`}
-          onClick={() => setTab("Completed")}
-        >
-          Completed
-        </a>
-        <a
-          className={`tab tab-bordered ${
-            tab === "Plan to Play" && "tab-active"
-          }`}
-          onClick={() => setTab("On Hold")}
-        >
-          Plan to Play
-        </a>
-        <a
-          className={`tab tab-bordered ${tab === "Dropped" && "tab-active"}`}
-          onClick={() => setTab("Dropped")}
-        >
-          Dropped
-        </a>
-      </div>
-      <div className="grid grid-cols-3 place-items-center my-4 ">
-        <div>plataform</div>
-        <div>
+    <>
+      {userList.length > 0 ? (
+        <div className="">
+          <div className="tabs flex-nowrap overflow-auto sm:justify-center">
+            <a
+              className={`tab tab-bordered ${tab === "All" && "tab-active"}`}
+              onClick={() => setTab("All")}
+            >
+              All
+            </a>
+            <a
+              className={`tab tab-bordered ${
+                tab === "Playing" && "tab-active"
+              }`}
+              onClick={() => setTab("Playing")}
+            >
+              Playing
+            </a>
+            <a
+              className={`tab tab-bordered ${
+                tab === "Completed" && "tab-active"
+              }`}
+              onClick={() => setTab("Completed")}
+            >
+              Completed
+            </a>
+            <a
+              className={`tab tab-bordered ${
+                tab === "Plan to Play" && "tab-active"
+              }`}
+              onClick={() => setTab("On Hold")}
+            >
+              Plan to Play
+            </a>
+            <a
+              className={`tab tab-bordered ${
+                tab === "Dropped" && "tab-active"
+              }`}
+              onClick={() => setTab("Dropped")}
+            >
+              Dropped
+            </a>
+          </div>
+          <div className="grid grid-cols-3 place-items-center my-4 ">
+            <div>plataform</div>
+            <div>
+              {tab !== "All"
+                ? userList.filter((list) => list.type === tab).length
+                : userList.length}
+            </div>
+            <div>order</div>
+          </div>
           {tab !== "All"
-            ? userList.filter((list) => list.type === tab).length
-            : userList.length}
+            ? userList
+                .filter((item) => item.type === tab)
+                .map((item) => <ListGameData item={item} key={item.game.id} />)
+            : userList.map((item) => (
+                <ListGameData item={item} key={item.game.id} />
+              ))}
         </div>
-        <div>order</div>
-      </div>
-      {tab !== "All"
-        ? userList
-            .filter((item) => item.type === tab)
-            .map((item) => <ListGameData item={item} key={item.game.id} />)
-        : userList.map((item) => (
-            <ListGameData item={item} key={item.game.id} />
-          ))}
-    </div>
+      ) : (
+        <EmptyList />
+      )}
+    </>
   );
 }
