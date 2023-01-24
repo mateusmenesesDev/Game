0588543be82f58df4a7378715aceb03d91063908
Mateus <mateusppa10@gmail.com>
2023-01-24 12:19:43 -0300

    :zap: perf: Tentando melhorar performance da page Home através da lib react-lazy-load

--- src/components/Genres/GameCarousel/index.tsx
+++ src/components/Genres/GameCarousel/index.tsx
@@ -1,18 +1,19 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation, Mousewheel } from "swiper";
-import { IBasicGameApi } from "../../../types/IGames";
-import { GameImage } from "../../GameImage";
-import { useContext } from "react";
-import { Context } from "../../../contexts/Context";
-import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/lazy';
+import { Navigation, Mousewheel } from 'swiper';
+import { IBasicGameApi } from '../../../types/IGames';
+import { GameImage } from '../../GameImage';
+import { useContext } from 'react';
+import { Context } from '../../../contexts/Context';
+import { Link } from 'react-router-dom';
 
 type Props = {
   genre: IBasicGameApi;
 };
 
-export function GameCarousel({ genre }: Props) {
+function GameCarousel({ genre }: Props) {
   const { games } = useContext(Context);
   const gamesByGenre = games.filter((game) => game.genres?.includes(genre.id));
   return (
@@ -26,14 +27,15 @@ export function GameCarousel({ genre }: Props) {
         breakpoints={{
           768: { slidesPerGroup: 2, slidesPerView: 3 },
           1024: { slidesPerGroup: 3, slidesPerView: 4 },
-          1440: { slidesPerGroup: 4, slidesPerView: "auto" },
+          1440: { slidesPerGroup: 4, slidesPerView: 'auto' },
         }}
-        className="pb-10 pt-2"
+        lazy={true}
+        className='pb-10 pt-2'
       >
         {gamesByGenre.map((game) => (
           <SwiperSlide
             key={game.id}
-            className="first:ml-4 md:first:ml-8 w-[310px] h-auto max-h-[440px] transition-all cursor-pointer lg:mt-6 lg:hover:scale-105 "
+            className='first:ml-4 md:first:ml-8 w-[310px] h-auto max-h-[440px] transition-all cursor-pointer lg:mt-6 lg:hover:scale-105 '
           >
             <Link to={`detail/${game.id}`}>
               <GameImage ImageId={game.cover.image_id} rounded hover />
@@ -44,3 +46,5 @@ export function GameCarousel({ genre }: Props) {
     </>
   );
 }
+
+export default GameCarousel;
