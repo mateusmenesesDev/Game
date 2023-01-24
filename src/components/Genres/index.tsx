4788a2d9e5f427ed2c8657cec166f6b16fc4627d
Mateus <mateusppa10@gmail.com>
2023-01-24 13:05:19 -0300

    :bug: fix: Tentativa de resolver problema de dependencia que causou bug no deploy

--- src/components/Genres/index.tsx
+++ src/components/Genres/index.tsx
@@ -1,27 +1,29 @@
-import "swiper/css";
-import "swiper/css/navigation";
-import "./Genre.css";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import './Genre.css';
 
-import { Placeholder } from "../utils/Placeholder";
-import { useContext } from "react";
-import { GameCarousel } from "./GameCarousel";
-import { Context } from "../../contexts/Context";
-
+import { Placeholder } from '../utils/Placeholder';
+import { useContext } from 'react';
+import { GameCarousel } from './GameCarousel';
+import { Context } from '../../contexts/Context';
+import LazyLoad from 'react-lazyload';
 export function Genres() {
   const { genres } = useContext(Context);
   return (
     <div>
       {genres.length > 0 ? (
         genres.map((genre) => (
-          <div key={genre.id}>
-            <h3
-              key={genre.id}
-              className="font-bold text-lg lg:text-3xl ml-4 md:ml-8"
-            >
-              {genre.name}
-            </h3>
-            <GameCarousel genre={genre} />
-          </div>
+          <LazyLoad offset={200} height={300} once key={genre.id}>
+            <div>
+              <h3
+                key={genre.id}
+                className='font-bold text-lg lg:text-3xl ml-4 md:ml-8'
+              >
+                {genre.name}
+              </h3>
+              <GameCarousel genre={genre} />
+            </div>
+          </LazyLoad>
         ))
       ) : (
         <Placeholder quantify={16} />
