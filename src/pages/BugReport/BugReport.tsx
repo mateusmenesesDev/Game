a41a8acfb6aef2acdf5ea570012c732f3f5e0d71
Luiz Felipe <luckpecontato@gmail.com>
2023-01-29 13:39:01 -0300

    feat: pagina de bug report

---
+++ src/pages/BugReport/BugReport.tsx
@@ -0,0 +1,41 @@
+
+const BugReportPage = () => {
+
+  return (
+    <form
+      action='https://formspree.io/f/mvonzrbb'
+      method='POST'
+      className='max-w-3xl text-base-content bg-base-300 p-6 rounded-lg shadow-md flex flex-col w-5/6 m-auto'
+    >
+      <h1 className='text-2xl font-medium mb-4'>Bug Report</h1>
+      <label className='block mb-4' htmlFor='bugDescription'>
+        <span className='font-medium'>Bug description:</span>
+        <textarea
+          name="bugDescription"
+          className='textarea mt-1 block w-full'
+          id='bugDescription'
+        />
+      </label>
+      <label className='block mb-4' htmlFor='bugSeverity'>
+        <span className=' font-medium'>Severity:</span>
+        <select
+          className='select mt-1 block w-full'
+          name="bugSeverity"
+          id='bugSeverity'
+        >
+          <option value='Low'>Low</option>
+          <option value='Medium'>Medium</option>
+          <option value='High'>High</option>
+        </select>
+      </label>
+      <button
+        className='bg-primary text-white py-2 px-4 rounded hover:bg-primary-focus'
+        type='submit'
+      >
+        Send Report
+      </button>
+    </form>
+  );
+};
+
+export default BugReportPage;
--- src/routes/RouteList.tsx
+++ src/routes/RouteList.tsx
@@ -1,4 +1,5 @@
 import { useRoutes } from 'react-router-dom';
+import BugReport from '../pages/BugReport/BugReport';
 import { Detail } from '../pages/Detail/Detail';
 import { Home } from '../pages/Home';
 import { List } from '../pages/List/List';
@@ -11,6 +12,7 @@ export function RouteList() {
     { path: '/detail/:gameId', element: <Detail /> },
     { path: '/list', element: <List /> },
     { path: '/login', element: <Login /> },
+    { path: '/report', element: <BugReport /> },
     { path: '*', element: <NotFound /> },
   ]);
 }
