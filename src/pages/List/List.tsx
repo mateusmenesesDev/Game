2fafb10128587a06e14ff7eaaf6822ae292ebe6d
Luiz Pereira <luckpecontato@gmail.com>
2023-03-08 14:33:39 -0300

    refactor: improved logic and cleaner code

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -6,58 +6,65 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/Auth';
 import { useContext, useEffect, useState } from 'react';
 import FilterDropdown from './FilterDropdown';
+import { IUserList } from '../../types/IGames';
+
+type FilterValue = 'highest' | 'lowest';
+
+const filterGames = (games: IUserList[], tab: string): IUserList[] => {
+  let filteredGames = games;
+
+  if (tab !== 'All') {
+    filteredGames = games.filter((game) => game.type === tab);
+  }
+
+  return filteredGames;
+};
 
 export function List() {
   const { userList } = useContext(Context);
   const { user } = useAuth();
   const [tab, setTab] = useState('All');
-  const [filterValue, setFilterValue] = useState('highest');
+  const [filterValue, setFilterValue] = useState<FilterValue>('highest');
   const [filteredUserList, setFilteredUserList] = useState(userList);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (user === null) {
+    if (!user) {
       navigate('../login');
     }
-  }, []);
+  }, [user]);
 
   useEffect(() => {
-    setFilteredUserList(userList);
-    if (tab !== 'All') {
-      setFilteredUserList(userList?.filter((item) => item.type === tab));
-    }
-  }, [filterValue, userList, tab]);
+    setFilteredUserList(filterGames(userList, tab));
+  }, [userList, tab, filterValue]);
 
-  return (
-    <div>
-      {!user?.emailVerified ? (
-        <div className='text-xl text-center'>Confirm your email first</div>
-      ) : filteredUserList && userList.length > 0 ? (
+  const sortedUserList = filteredUserList
+  .sort((a, b) => {
+    return filterValue === 'lowest'
+      ? a.rating - b.rating
+      : b.rating - a.rating;
+  })
+
+  const displayContent = () => {
+     if (sortedUserList.length > 0 || tab !== 'All') {
+      return (
         <>
           <ListTabs tab={tab} setTab={setTab} />
           <div className='grid grid-cols-3 place-items-center my-4 '>
-            <div>
-              {tab !== 'All'
-                ? filteredUserList.filter((list) => list.type === tab).length
-                : filteredUserList.length}
-            </div>
+            <div>{sortedUserList.length}</div>
             <FilterDropdown setFilterValue={setFilterValue} />
           </div>
           <div>
-            {filteredUserList
-              .sort((a, b) => {
-                return filterValue === 'lowest'
-                  ? a.rating - b.rating
-                  : b.rating - a.rating;
-              })
-              .map((item) => (
-                <ListGameData item={item} key={item.game.id} />
-              ))}
+            {sortedUserList.map((item) => (
+              <ListGameData item={item} key={item.game.id} />
+            ))}
           </div>
         </>
-      ) : (
-        <EmptyList />
-      )}
-    </div>
-  );
+      );
+    } else {
+      return <EmptyList />;
+    }
+  };
+
+  return <div>{displayContent()}</div>;
 }
